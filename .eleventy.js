a100abb91142ebf1c64031dc3faae4628e00edf5
Ken Chin-Purcell <ken@chin-purcell.org>
2025-02-11 21:42:33 -0800

    Bulma, images

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,22 @@
 import { execSync } from "child_process";
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 
 export default function (eleventyConfig) {
+  eleventyConfig.addPlugin(eleventyImageTransformPlugin);
+
   eleventyConfig.addPassthroughCopy({ "src/static": "." });
+  // eleventyConfig.addPassthroughCopy("src/**/*.{jpg,png,gif,svg,webp}");
+
+  eleventyConfig.addWatchTarget("src/static/site.css");
 
   return {
+    markdownTemplateEngine: "njk",
     templateFormats: ["html", "md", "njk"],
     dir: {
       input: "src",
       output: "_site",
+      layouts: "_layouts",
+      includes: "_includes",
     },
   };
 }
---
+++ src/post/foam/index.md
@@ -0,0 +1,12 @@
+---
+tags: food
+title: "The Quest for Microfoam"
+---
+
+Sometimes I've gotten nice smooth foam on the top of my soy milk.
+Other times it's all bubbly and uneven:
+
+<img alt="mega foam" src="./megafoam.jpeg" width=800>
+
+But YouTube to the rescue, I think I have some tricks to pull next time
+I froth my morning fluffy drinks.
